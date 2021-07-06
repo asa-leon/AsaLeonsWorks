b791a79a65169932b24fd3f4a183dff51a83e79a
ko_higa <ko_higa@icloud.com>
2021-07-06 22:38:06 +0900

    Made a part of 'top-menu' into as a component for Next. And changed a file to render its compnent in 'pages/top-menu.js'.

--- components/top-menu.js
+++ components/top-menu.js
@@ -1,32 +1,33 @@
 import Link from 'next/link'
-import styles from './top-menu.module.css'
+import styles from '/components/top-menu.module.css'
 
 // トップメニューのレイアウトを含むコンポーネント
 
 export default function TopMenu() {
 	return (
 
-		<nav id="top-menu">
-			<ul class="nav nav-tabs" id="topMenuTab" role="tablist">
+		<nav className={styles.TopMenu}>
+			<ul className="nav nav-tabs" id="topMenuTab" role="tablist">
 				
-				<li class="nav nav-tabs">
+				<li className="col nav-item">
 					<Link href="#frontend">
-						<a class="nav-link active" id="frontend-tab" data-toggle="tab" role="tab" aria-controls="frontend" aria-selected="true">Frontend Coding</a>
+						<a className="nav-link active" id="frontend-tab" data-toggle="tab" role="tab" aria-controls="frontend" aria-selected="true">Frontend Coding</a>
 					</Link>
 				</li>
 
-				<li class="nav nav-tabs">
+				<li className="col nav-item">
 					<Link href="#php">
-					<a class="nav-link" id="php-tab" data-toggle="tab" role="tab" aria-controls="php" aria-selected="false">PHP</a>
+					<a className="nav-link" id="php-tab" data-toggle="tab" role="tab" aria-controls="php" aria-selected="false">PHP</a>
 					</Link>
 				</li>
 
-				<li class="nav nav-tabs">
+				<li className="col nav-item">
 					<Link href="#beatmaking">
-					<a class="nav-link" id="beatmaking-tab" data-toggle="tab" role="tab" aria-controls="beatmaking" aria-selected="false">Beat Making</a>
+					<a className="nav-link" id="beatmaking-tab" data-toggle="tab" role="tab" aria-controls="beatmaking" aria-selected="false">Beat Making</a>
 					</Link>
 				</li>
 			</ul>
 		</nav>
+
 	)
 }
--- pages/top-menu.js
+++ pages/top-menu.js
@@ -1,33 +1,13 @@
 import Link from 'next/link'
 import styles from '/components/top-menu.module.css'
+import EnbedTopMenu from '../components/top-menu'
 
 // トップメニューのレイアウトを含むコンポーネント
 
 export default function TopMenu() {
 	return (
 
-		<nav class="top-menu">
-			<ul class="nav nav-tabs" id="topMenuTab" role="tablist">
-				
-				<li class="nav nav-tabs">
-					<Link href="#frontend">
-						<a class="nav-link active" id="frontend-tab" data-toggle="tab" role="tab" aria-controls="frontend" aria-selected="true">Frontend Coding</a>
-					</Link>
-				</li>
+		<EnbedTopMenu></EnbedTopMenu>
 
-				<li class="nav nav-tabs">
-					<Link href="#php">
-					<a class="nav-link" id="php-tab" data-toggle="tab" role="tab" aria-controls="php" aria-selected="false">PHP</a>
-					</Link>
-				</li>
-
-				<li class="nav nav-tabs">
-					<Link href="#beatmaking">
-					<a class="nav-link" id="beatmaking-tab" data-toggle="tab" role="tab" aria-controls="beatmaking" aria-selected="false">Beat Making</a>
-					</Link>
-				</li>
-			</ul>
-		</nav>
-		
 	)
 }
