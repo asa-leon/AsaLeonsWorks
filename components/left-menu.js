42c7f3c85eb27aada94013b33bafcef3dbeaf959
ko_higa <ko_higa@icloud.com>
2021-07-09 15:49:10 +0900

    Done coding 'left-menu.js' with is 'module.css'.

--- components/left-menu.js
+++ components/left-menu.js
@@ -12,32 +12,31 @@ export default function LeftMenu() {
 	
 	return (
 
-            <Col className={'toLeft col-md-3 col-sm-5 vh-100'}>
+		<div className={styles.toLeft}>
+			<div class="vh-100">
+            <Nav defaultActiveKey="frontend" className="flex-column">
+
 				<div className={styles.leftMenuTitle}>
-					<img 
-					src="/images/folderIcon.png" 
-					alt="icon of folder" 
-					/>
+					<img src="/images/folderIcon.png" alt="icon of folder" />
 					<h1>Leon Mah's Work</h1>
 				</div>
-				<Nav className={'d-none d-sm-block flex-column'} variant="pills" defaultActiveKey={"frontend"}>
-					
-                    <Nav.Item className={styles.leftMenuNavItems}>
-						<img src="/images/htmlIcon.png" alt="icon of html"/>
-                    	<Nav.Link href="#frontend" >Frontend Coding</Nav.Link>
-                    </Nav.Item>
-                
-                
-                	<Nav.Item className={styles.leftMenuNavItems}>
-						<img src="/images/phpIcon.png" alt="icon of html"/>
-                		<Nav.Link eventKey="php">PHP</Nav.Link>
-                	</Nav.Item>
-                
-					<Nav.Item className={styles.leftMenuNavItems}>
-					<img src="/images/beatMakingIcon.png" alt="icon of html"/>
-						<Nav.Link eventKey="beatmaking">Beat Making</Nav.Link>
-					</Nav.Item>
-				</Nav>
-            </Col>
+
+				<Nav.Item className={styles.leftMenuNavItems}>
+					<img src="/images/htmlIcon.png" alt="icon of html" />
+					<Nav.Link href="#frontend">Frontend Coding</Nav.Link>
+				</Nav.Item>
+				
+				<Nav.Item className={styles.leftMenuNavItems}>
+					<img src="/images/phpIcon.png" alt="icon of php" />
+					<Nav.Link href="#php">PHP</Nav.Link>
+				</Nav.Item>
+
+				<Nav.Item className={styles.leftMenuNavItems}>
+					<img src="/images/beatMakingIcon.png" alt="icon of html" />
+					<Nav.Link href="#beatMaking">Beat Making</Nav.Link>
+				</Nav.Item>
+			</Nav>
+			</div>
+			</div>
 	)
 }
