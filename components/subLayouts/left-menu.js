d48c1798e7966456139979899f8d1beeddaa33a9
ko_higa <ko_higa@icloud.com>
2021-07-15 01:30:50 +0900

    Changed some CSS for 'left-menu', but stack in how to change the link overriding bootstrap.

--- components/subLayouts/left-menu.js
+++ components/subLayouts/left-menu.js
@@ -16,24 +16,23 @@ export default function LeftMenu() {
 				<img src="/images/folderIcon.png" alt="icon of folder" />
 				<h1>Leon Mah's Work</h1>
 			</div>
-			<Nav 
+			<Nav
 			activeKey={titleNum} 
 			className="flex-column"
 			onSelect={ (k) => setTitleNum(k) }
 			>
 				<Nav.Item className={styles.leftMenuNavItems}>
-					<img src="/images/htmlIcon.png" alt="icon of html" />
-					<Nav.Link eventKey={0}>Frontend Coding</Nav.Link>
+					<Nav.Link 
+						className={styles.leftMenuNavLink}
+						eventKey={0}><img src="/images/htmlIcon.png" alt="icon of html" />Frontend Coding</Nav.Link>
 				</Nav.Item>
 
 				<Nav.Item className={styles.leftMenuNavItems}>
-					<img src="/images/phpIcon.png" alt="icon of php" />
-					<Nav.Link eventKey={1}>PHP</Nav.Link>
+					<Nav.Link eventKey={1}><img src="/images/phpIcon.png" alt="icon of php" />PHP</Nav.Link>
 				</Nav.Item>
 
 				<Nav.Item className={styles.leftMenuNavItems}>
-					<img src="/images/beatMakingIcon.png" alt="icon of html" />
-					<Nav.Link eventKey={2}>Beat Making</Nav.Link>
+					<Nav.Link eventKey={2}><img src="/images/beatMakingIcon.png" alt="icon of html" />Beat Making</Nav.Link>
 				</Nav.Item>
 			</Nav>
 		</>
