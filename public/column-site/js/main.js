280fbbbd6c484e065b1092656b7d006923c6506f
ko_higa <ko_higa@icloud.com>
2021-07-27 17:30:01 +0900

    Stack in 'leftNavMenu's movement by jQuery.

--- public/column-site/js/main.js
+++ public/column-site/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     // goTopのスクロール機能
-    $(function scrollToTop() {
+    $(function () {
         $('.goTop').click( function(event) {
             $('html, body').animate({
                 scrollTop: 0
@@ -13,19 +13,18 @@ $(document).ready(function(){
     });
 
     // コラムページのleft-menu-navの挙動
-    $(window).scroll(function leftMenuMovement() {
-        let screenHeight = window.screen.height
-        let navOffset = $('#leftMenuNav').offset();
+    $(window).on('scroll', function () {
+        let $window = $(window),
+        $nav = $('#leftMenuNav'),
+        defaultPositionTop = $('header').height(),
+        setOffsetPosition = $nav.offset(),
+        fixedClassName = 'fixed';
 
-        let offsetX = navOffset.left;
-        let offsetY = navOffset.top;
-        console.log(offsetY);
-
-        if (offsetY > $('header').height) {
-            console.log("over header");
-
-            //offsetY = 
-        };
-
+        if ($(this).scrollTop() > defaultPositionTop) {
+            $nav.addClass(fixedClassName).css('top', defaultPositionTop + $(this).scrollTop() - defaultPositionTop + 'px');
+        } else {
+            $('.' + fixedClassName).css('top', 0);
+            $nav.removeClass(fixedClassName);
+        }
     });
 });
