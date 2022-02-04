3a50c00e2dfc2c9e2ecb669cf8685f8846db2182
ko_higa <ko_higa@icloud.com>
2022-02-04 22:15:20 +0900

    Edit "package.json" and "next.config.js" for usability of local develop.

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-module.exports = {
-  reactStrictMode: true,
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      /* development only config options here */
+      reactStrictMode: true,
+    }
+  }
+
+  return {
+    /* config options for all phases except development here */
+    reactStrictMode: false,
+  }
 }
