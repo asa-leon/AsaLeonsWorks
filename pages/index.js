2d2df6f681a5ec714227030669aa5fcf6859b239
ko_higa <ko_higa@icloud.com>
2022-05-16 11:40:45 +0900

    Starting point; try to code rate limiter.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Layout from '../components/layout'
 
 export default function Home() {
+
+	const [response, setResponse] = useState()
+
+	const makeRequest = async () => {
+		const res = await fetch('/api/hello')
+
+		setResponse({
+			status: res.status,
+			body: await res.json(),
+			limit: res.headers.get('X-RateLimit-Limit'),
+			remaining: res.headers.get('X-RateLimit-Remaining'),
+		})
+	}
   return (
     <Layout home>
       
