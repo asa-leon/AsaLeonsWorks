749bac32110ba50f91edee2c23264abe65729276
ko_higa <ko_higa@icloud.com>
2022-05-18 13:27:45 +0900

    Add; rate limit files but it will not work now.

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ const securityHeaders = [
 	},
 	{
 		key: 'Permissions-Policy',
-		value: 'camera=(), microphone=(), geolocatioin=()'
+		value: 'camera=(), microphone=(), geolocation=()'
 	},
 	{
 		key: 'Referrer-Policy',
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+import requestIp from 'request-ip'
+import rateLimit from '../../libs/rate-limit'
+
+const limiter = rateLimit({
+	interval: 60 * 1000,
+	uniqueTokenPerInterval: 500,
+})
+
+export default async function handler(req, res) {
+
+	const clientIp = requestIp.getClientIp(req) || 'IP_NOT_FOUND'
+
+	try {
+
+		await limiter.check(res, 10, clientIp)
+		
+		
+	} catch (error) {
+		
+		console.error(error)
+
+		res.status(429).json({ 
+			text: `Rate Limited`,
+			clientIp: clientIp
+		})
+		return
+	}
+
+	res.status(200).json({ 
+		text: `テキスト`,
+		clientIp: clientIp
+	})
 }
