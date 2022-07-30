b91ad2f32ec1f630dc86981b0235fd444aca65d6
ko_higa <ko_higa@icloud.com>
2022-07-31 01:04:45 +0900

    Edit; made package.json, pm2Dev..., serverOnLocal.js to be capable of serving node server.

---
+++ pm2Dev.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	apps: [
+			{
+					name: "Asa Leon's Works(8443) -dev",
+					script: "npm",
+					automation: false,
+					args: "run start-dev",
+					cwd: '/usr/share/nginx/asa-leon.works/',
+					env: {
+							PORT: 8443,
+							NODE_ENV: "development"
+					},
+					env_production: {
+							PORT: 8443,
+							NODE_ENV: "production",
+					},
+					watch: ["server", "client"],
+					// Delay between restart
+					ignore_watch: ["node_modules", "log"],
+			}
+	]
+}
--- serverOnLocal.js
+++ serverOnLocal.js
@@ -3,8 +3,8 @@ const next = require('next')
 const https = require('https')
 const fs = require('fs')
 
-const port = parseInt(process.env.PORT || '4300')
-const host = 'localhost'
+const port = parseInt(process.env.PORT || '8443')
+const host = '0.0.0.0'
 const dev = process.env.NODE_ENV !== 'production'
 
 const app = next({ dev, port, host })
@@ -19,17 +19,17 @@ app.prepare().then(async () => {
 
 	// Use https if https option enabled
 	const hasCertificates = 
-		fs.existsSync('./certificates/localhost-key.pem') &&
-		fs.existsSync('./certificates/localhost.pem')
+		fs.existsSync('../certificates/164.70.103.171-key.pem') &&
+		fs.existsSync('../certificates/164.70.103.171.pem')
 
-	const useHttps =
-		process.env.HTTPS === 'true' && hasCertificates
+	//const useHttps =
+	//	process.env.HTTPS === 'true' && hasCertificates
 
-	if (useHttps) {
+	if (hasCertificates) {
 
 		const options = {
-			key: fs.readFileSync('./certificates/localhost-key.pem'),
-			cert: fs.readFileSync('./certificates/localhost.pem')
+			key: fs.readFileSync('../certificates/164.70.103.171-key.pem'),
+			cert: fs.readFileSync('../certificates/164.70.103.171.pem')
 		}
 
 		const server = https.createServer(options, expressApp)
