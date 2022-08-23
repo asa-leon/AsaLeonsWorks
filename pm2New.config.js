5841b746cadcf39f3ef9d720663822c8bef38474
ko_higa <ko_higa@icloud.com>
2022-08-23 15:18:23 +0900

    Add; ecosystem but never be commit, newServer.js to start new server. Edit; package.json to handle new server.

---
+++ pm2New.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	apps: [
+			{
+					name: "Asa Leon's Works(8443) -Production",
+					script: "npm",
+					automation: false,
+					args: "run start-newServer",
+					cwd: '/usr/share/nginx/html/asa-leon.works/',
+					env: {
+							PORT: 8443,
+							NODE_ENV: "development"
+					},
+					env_production: {
+							PORT: 8443,
+							NODE_ENV: "production",
+					},
+					watch: ["server", "client"],
+					// Delay between restart
+					ignore_watch: ["node_modules", "log"],
+			}
+	]
+}
